6d1cfb2132c5e1dd22f0b2220692b7423ac56910
studentemarco <160661760+studentemarco@users.noreply.github.com>
2025-11-27 18:44:42 +0100

    .

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-    session_start();
-    //session_unset(); // svuota tutte le variabili di sessione
-    unset($_SESSION['name']);
-    unset($_SESSION['color']);
-    session_destroy();
-    header("location: index.php");
-    exit();
-?>
